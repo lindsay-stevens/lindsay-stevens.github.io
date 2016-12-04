ae8c42c7fd0f2a61d0bd1630797409e2eea1ba37
lindsay.stevens.au <lindsay.stevens.au@gmail.com>
2016-12-04 12:58:18 +1100

    add: footer to refdocx, isodate with resume title
    - footer to help identify doc
    - date to help separate versions

--- pandoc/resume_to_docx.bat
+++ pandoc/resume_to_docx.bat
@@ -1,4 +1,5 @@
+for /F "delims=" %%i IN ('powershell -Command "Get-Date -format yyyy-MM-dd"') DO set right_now=%%i
 pandoc %~dp0../markdown/resume.md -o %~dp0../docx/resume.docx                  ^
-  -M title="Resume" --toc --toc-depth 2 --number-sections                      ^
-  --reference-docx=%~dp0reference.docx
+  -M title="Resume - Lindsay Stevens" --toc --toc-depth 2 --number-sections    ^
+  --reference-docx=%~dp0reference.docx -M date="%right_now%"
 start %~dp0../docx/resume.docx
