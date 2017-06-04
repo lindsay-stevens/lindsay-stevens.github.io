61853f649ab87266845031939af5e6d9caa81622
Lindsay Stevens <lindsay.stevens.au@gmail.com>
2017-06-05 00:01:47 +1000

    chg: remove index, update resume

--- pandoc/resume_to_html.bat
+++ pandoc/resume_to_html.bat
@@ -1,6 +1,5 @@
 :: Generate updated HTML from Markdown.
-pandoc %~dp0../markdown/resume.md -o %~dp0../html/resume.html                  ^
-  --template %~dp0template.html --css ../css/styles.css                        ^
-  -M title="Resume" --toc --toc-depth 2 --number-sections                      ^
-  -V home-link
+pandoc %~dp0../markdown/index.md -o %~dp0../index.html                         ^
+  --template %~dp0template.html --css css/styles.css                           ^
+  -M title="Resume" --toc --toc-depth 2 --number-sections
 
--- pandoc/update_index.bat
+++
@@ -1,4 +0,0 @@
-:: Generate index. No table of contents, different CSS path.
-pandoc %~dp0../markdown/index.md -o %~dp0../index.html                         ^
-  --template %~dp0template.html --css css/styles.css                        ^
-  -M title="Home" --toc --toc-depth 2 --number-sections
