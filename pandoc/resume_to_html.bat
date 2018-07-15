a978a1e7bd3b0ccac6ba293c2d8a983cd79b9402
lindsay-stevens <lindsay.stevens.au@gmail.com>
2018-07-16 04:48:55 +1000

    chg: new style + services page, updated other minor things

--- pandoc/resume_to_html.bat
+++
@@ -1,5 +0,0 @@
-:: Generate updated HTML from Markdown.
-pandoc %~dp0../markdown/index.md -o %~dp0../index.html                         ^
-  --template %~dp0template.html --css css/styles.css                           ^
-  -M title="Resume" --toc --toc-depth 2 --number-sections
-
